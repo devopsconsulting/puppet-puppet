b4a859663861212d84497da87237fce22ea7a0a8
Martijn Jacobs <martijn@fourdigits.nl>
2012-10-08 16:17:54 +0200

    Typo

--- manifests/version.pp
+++ manifests/version.pp
@@ -1,6 +1,6 @@
 # last tested version : 2.7.19
 class puppet::version {
-    package {"puppet-common"} { 
+    package {"puppet-common": { 
         ensure => "2.7.19-1puppetlabs2"
     }
     package {"puppet":
